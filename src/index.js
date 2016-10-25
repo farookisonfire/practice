450f523b294fd8e0a9d31325f23bf14bd03541db
Farook Khan <farook.khan@gmail.com>
2016-10-25 16:05:02 -0700

    Editing source code to reference PORT and MONGODB_URI as environment variables

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const { MongoClient } = require('mongodb')
 const createApp = require('./express')
 
 const DEFAULT_MONGO_URI = 'mongodb://localhost:27017/six'
-const MONGO_URI = process.env.MONGO_URI || DEFAULT_MONGO_URI
+const MONGODB_URI = process.env.MONGODB_URI || DEFAULT_MONGO_URI
 const PORT =  process.env.PORT ||3001
 
-MongoClient.connect(MONGO_URI, (err, db) => {
+MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error(err)
     process.exit(1)
