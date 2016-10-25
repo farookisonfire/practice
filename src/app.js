ae1d86175eafedc5b6568478f5ff6958ee65caf8
Farook Khan <farook.khan@gmail.com>
2016-10-25 16:26:34 -0700

    Issue #1 - added start script for Heroku deployment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const { Provider } = require('react-redux')
-const MainComponent = require('./components/container')
+const MainComponent = require('./components/main-component')
 const store = require('./store')
 
 ReactDOM.render(
